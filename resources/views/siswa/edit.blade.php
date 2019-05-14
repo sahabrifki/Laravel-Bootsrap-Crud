4499f5a4e1017f29aae2bd5c0e96ff60a4dab720
sahabrifki <duniaaguaa@gmail.com>
2019-05-14 11:20:55 +0700

    fix @index.blade.php >> tambah data views

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -5,20 +5,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
   <title>Laravel-DataSiswa</title>
 </head>
+
 <body>
+
   <div class="container">
         <h1>Edit Data Siswa</h1>
         @if(session('sukses'))
         <div class="alert alert-success" role="alert">
           {{session('sukses')}}
-        </div>
+  </div>
         @endif
 
     <!--Form-->
-    <form action="/siswa/{{$siswa->id}}/update" method="POST">
+    <form action="/siswa/{{$siswa->id}}/edit" method="POST">
         {{csrf_field()}}
 
           <div class="form-group">
@@ -52,13 +53,6 @@
           <button type="submit" class="btn btn-warning">Edit data</button>
 
         </form>
-
-
-      </div>
-    </div>
-  </div>
-</div>
-
         
             
 
--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -20,55 +20,55 @@
             <div class="row">
                 <div class="col-6">
                     <h1>Laravel-DataSiswa</h1>
-                </div>
+            </div>
                 
-                <div class="col-6">
-                    <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#exampleModalLong">
-                          Tambah Data Siswa
-                        </button>
-                </div>
+            <div class="col-6">
+                <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#exampleModalLong">
+                      Tambah Data Siswa
+                    </button>
+            </div>
 
-                    <table class="table table-hover">
+            <table class="table table-hover">
+        
+                <tr>
+                    <th>#</th>
+                    <th>ID</th>
+                    <th>NAMA DEPAN</th>
+                    <th>NAMA BELAKANG</th>
+                    <th>JENIS KELAMIN</th>
+                    <th>AGAMA</th>
+                    <th>ALAMAT</th>
+                    <th>Opsi</th>
+                </tr>
                 
-                        <tr>
-                            <th>#</th>
-                            <th>ID</th>
-                            <th>NAMA DEPAN</th>
-                            <th>NAMA BELAKANG</th>
-                            <th>JENIS KELAMIN</th>
-                            <th>AGAMA</th>
-                            <th>ALAMAT</th>
-                            <th>Opsi</th>
-                        </tr>
-                        
-                        <?php $row = 0 ?>
-                        @foreach($data_siswa as $siswa)
-                        <?php $row++ ?>
-                        
-                        <tr>
-                            <td>{{$row}}</td>
-                            <td>{{$siswa->id}}</td>
-                            <td>{{$siswa->nama_depan}}</td>
-                            <td>{{$siswa->nama_belakang}}</td>
-                            <td>{{$siswa->jenis_kelamin}}</td>
-                            <td>{{$siswa->agama}}</td>
-                            <td>{{$siswa->alamat}}</td>
-                            <td><a href="/siswa/{{$siswa->id}}/edit" class='btn btn-warning'>Edit</a></td>
+                <?php $row = 0 ?>
+                @foreach($data_siswa as $siswa)
+                <?php $row++ ?>
+                
+                <tr>
+                    <td>{{$row}}</td>
+                    <td>{{$siswa->id}}</td>
+                    <td>{{$siswa->nama_depan}}</td>
+                    <td>{{$siswa->nama_belakang}}</td>
+                    <td>{{$siswa->jenis_kelamin}}</td>
+                    <td>{{$siswa->agama}}</td>
+                    <td>{{$siswa->alamat}}</td>
+                    <td><a href="/siswa/{{$siswa->id}}/edit" class='btn btn-warning'>Edit</a></td>
 
-              
-                        </tr>
-                        @endforeach
-                    
-                    
-                    </table>
+      
+                </tr>
+                @endforeach
+            
+            
+            </table>
                 
             </div>
         </div>
 
 
 <!-- Modal Bootsrap -->
-  <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -77,6 +77,7 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
+      <div class="modal-body">
 
 <!--Form-->
       <form action="/siswa/create" method="POST">
@@ -119,6 +120,7 @@
     </div>
   </div>
 </div>
+
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
